21121b5d5aa9a0265997a48525b6ad5964d0ec62
sekhoniqbal <42016178+sekhoniqbal@users.noreply.github.com>
2018-12-24 10:24:32 -0600

    newcalc-workcommit

--- sw.js
+++ sw.js
@@ -5,10 +5,17 @@ var test=[];
 self.addEventListener("install", async function(event){
     var cache = await caches.open("calcCache");
     cache.addAll(staticAssets);
+	showInstallButton(true);
     console.log("ran this once");
 });
 self.addEventListener("fetch", function(event){
     const req = event.request;
-    event.respondWith(caches.match(req)||fetch(req));
+    event.respondWith(getRes(req));
     
 });
+async function getRes(req){
+ const res = await caches.match(req);
+ if(res){return res}
+ return fetch(req);
+ 
+}
